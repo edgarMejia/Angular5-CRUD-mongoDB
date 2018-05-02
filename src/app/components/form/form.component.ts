2dce4e16af69391c9abc870adf480acf77963c37
Edgar Mejía <edgar13155@gmail.com>
2018-05-01 23:27:13 -0600

    Correcciones.

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -37,12 +37,24 @@ export class FormComponent {
         });
     }
 
+    save(form:FormGroup){
+        console.log(form.value);
+        this.personService.save(form.value).subscribe((data:any) => {
+            if(data.success){
+                this.dialogRef.close(true);
+                this.openSnack(data);
+            }else{
+                this.openSnack(data);
+            }
+        });
+    }
+
     initializeForm(){
         if(this.data.action=='edit'){
             this.frm = this.fb.group({
                 firstName: new FormControl(this.data.data.firstName, [Validators.required, Validators.minLength(3)]),
                 lastName: new FormControl(this.data.data.lastName, [Validators.required, Validators.minLength(4)]),
-                email: new FormControl(this.data.data.email, [Validators.required, Validators.minLength(6)]),
+                email: new FormControl(this.data.data.email, [Validators.required, Validators.pattern("[^ @]*@[^ @]*")]),
                 department: new FormControl(this.data.data.department, [Validators.required, Validators.minLength(4)]),
                 dui: new FormControl(this.data.data.dui, [Validators.required, Validators.minLength(10)]),
                 nit: new FormControl(this.data.data.nit, [Validators.required, Validators.minLength(10)]),
@@ -61,7 +73,7 @@ export class FormComponent {
             this.frm = new FormGroup({
                 firstName: new FormControl(null, [Validators.required, Validators.minLength(3)]),
                 lastName: new FormControl(null, [Validators.required, Validators.minLength(4)]),
-                email: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+                email: new FormControl(null, [Validators.required, Validators.pattern("[^ @]*@[^ @]*")]),
                 department: new FormControl(null, [Validators.required, Validators.minLength(4)]),
                 dui: new FormControl(null, [Validators.required, Validators.minLength(10)]),
                 nit: new FormControl(null, [Validators.required, Validators.minLength(10)]),
@@ -79,52 +91,40 @@ export class FormComponent {
         }
     }
 
-    save(form:FormGroup){
-        console.log(form.value);
-        this.personService.save(form.value).subscribe((data:any) => {
-            if(data.success){
-                this.dialogRef.close(true);
-                this.openSnack(data);
-            }else{
-                this.openSnack(data);
-            }
-        });
-    }
-
     getFirstNameErrorMessage() {
-        return this.frm.controls.firstName.hasError('required') ? 'El campo Nombre es obligatorio' :
-        this.frm.controls.firstName.hasError('minlength') ? 'Al menos 2 caracteres' : '';
+        return this.frm.controls.firstName.hasError('required') ? 'The First Name field is required' :
+        this.frm.controls.firstName.hasError('minlength') ? 'At least 3 characters' : '';
     }
     getLastNameErrorMessage() {
-        return this.frm.controls.lastName.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.lastName.hasError('required') ? 'The Last Name field is required' :
         this.frm.controls.lastName.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getEmailErrorMessage() {
-        return this.frm.controls.email.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.email.hasError('required') ? 'The Email field is required' :
         this.frm.controls.email.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getDepartmentErrorMessage() {
-        return this.frm.controls.department.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.department.hasError('required') ? 'The Department field is required' :
         this.frm.controls.department.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getDuiErrorMessage() {
-        return this.frm.controls.dui.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.dui.hasError('required') ? 'The DUI field is required' :
         this.frm.controls.dui.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getNitErrorMessage() {
-        return this.frm.controls.nit.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.nit.hasError('required') ? 'The NIT field is required' :
         this.frm.controls.nit.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getCellphoneErrorMessage() {
-        return this.frm.controls.cellphone.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.cellphone.hasError('required') ? 'The Cellphone field is required' :
         this.frm.controls.cellphone.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getTelephoneErrorMessage() {
-        return this.frm.controls.telephone.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.telephone.hasError('required') ? 'The Telephone field is required' :
         this.frm.controls.telephone.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getBirthDateErrorMessage() {
-        return this.frm.controls.birthDate.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.birthDate.hasError('required') ? 'The Birth date field is required' :
         this.frm.controls.birthDate.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getGenderErrorMessage() {
@@ -132,22 +132,22 @@ export class FormComponent {
     }
     getLenguagesErrorMessage() {
         return this.frm.controls.lenguages.hasError('required') ? 'El campo Nombre es obligatorio' :
-        this.frm.controls.lenguages.hasError('minlength') ? 'Al menos 2 caracteres' : '';
+        this.frm.controls.lenguages.hasError('minlength') ? '' : '';
     }
     getAddressErrorMessage() {
-        return this.frm.controls.address.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.address.hasError('required') ? 'The Address field is required' :
         this.frm.controls.address.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getEmergencyContactErrorMessage() {
-        return this.frm.controls.emergencyContact.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.emergencyContact.hasError('required') ? 'The Emergency Contact field is required' :
         this.frm.controls.emergencyContact.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getLicenceNumberErrorMessage() {
-        return this.frm.controls.licenceNumber.hasError('required') ? 'El campo Nombre es obligatorio' :
+        return this.frm.controls.licenceNumber.hasError('required') ? 'The Licence Number field is required' :
         this.frm.controls.licenceNumber.hasError('minlength') ? 'Al menos 2 caracteres' : '';
     }
     getHireOnErrorMessage() {
-        return this.frm.controls.hireOn.hasError('required') ? 'El campo edad es obligatorio' :
+        return this.frm.controls.hireOn.hasError('required') ? 'The Hire On field is required' :
         this.frm.controls.hireOn.hasError('minlength') ? 'Al menos un numero debe ser ingresado' : '';
     }
 }
